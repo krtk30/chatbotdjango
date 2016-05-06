79b738c33eecb63c1ec3f9c676af3cacb0639072
Akshar Raaj <akshar@agiliq.com>
2016-05-06 15:47:39 +0530

    added a model

---
+++ cat/models.py
@@ -0,0 +1,8 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+class Course(models.Model):
+    name = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    time = models.CharField(max_length=255)
