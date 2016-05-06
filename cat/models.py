7faf073a36f9a3668fda43012b0108d97f80b955
Akshar Raaj <akshar@agiliq.com>
2016-05-06 19:07:22 +0530

    Added a model

--- cat/models.py
+++ cat/models.py
@@ -9,3 +9,13 @@ class Course(models.Model):
 
     def __unicode__(self):
         return self.name
+    
+
+class Term(models.Model):
+    name = models.CharField(max_length=255)
+    term_start_dt = models.DateField()
+    term_end_dt = models.DateField()
+    cat_yr = models.IntegerField()
+
+    def __unicode__(self):
+        return self.name
