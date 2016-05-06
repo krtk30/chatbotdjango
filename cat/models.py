34f6fee6e1125617a267a43635a5ad70048bfcd9
Akshar Raaj <akshar@agiliq.com>
2016-05-06 15:52:22 +0530

    added unicode

--- cat/models.py
+++ cat/models.py
@@ -6,3 +6,6 @@ class Course(models.Model):
     name = models.CharField(max_length=255)
     location = models.CharField(max_length=255)
     time = models.CharField(max_length=255)
+
+    def __unicode__(self):
+        return self.name
